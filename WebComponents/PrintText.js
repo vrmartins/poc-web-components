c1ae16f62310f534b88b32363e057f17a6721c3a
Vilmar Rodrigues Martins <vilmar.rodrigs@gmail.com>
2019-07-13 15:19:25 -0300

    chore: ESLint

--- WebComponents/PrintText.js
+++ WebComponents/PrintText.js
@@ -1,19 +1,19 @@
 class PrintText extends HTMLElement {
-  static get observedAttributes() {
-    return ['text'];
+  static get observedAttributes () {
+    return ['text']
   }
 
-  constructor() {
-    super();
+  constructor () {
+    super()
 
-    const shadow = this.attachShadow({mode: 'open'});
+    const shadow = this.attachShadow({ mode: 'open' })
 
     console.log(shadow)
 
-    const p = document.createElement('p');
+    const p = document.createElement('p')
     p.append(this.getAttribute('text'))
     shadow.appendChild(p)
   }
 }
 
-customElements.define('print-text', PrintText);
+customElements.define('print-text', PrintText)
